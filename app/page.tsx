4cf67860ab8dad4abb8c2c15cccbc218783c2e96
ClaytonDewey <clay@dryadmedia.com>
2024-01-26 16:24:21 -0600

    style: Work on about section

--- app/page.tsx
+++ app/page.tsx
@@ -43,34 +43,59 @@ export default function Home() {
         </div>
       </section>
 
-      <section>
-        <Image
-          src='/assets/home/desktop/illustration-passionate.svg'
-          role='presentation'
-          alt=''
-          height={202}
-          width={202}
-        />
-        <h2>Passionate</h2>
-        <p>
-          Each project starts with an in-depth brand research to ensure we only
-          create products that serve a purpose. We merge art, design, and
-          technology into exciting new solutions.
-        </p>
+      <section className='section__about'>
+        <div>
+          <div className='icon__container icon__container--large'>
+            <Image
+              src='/assets/home/desktop/illustration-passionate.svg'
+              role='presentation'
+              alt=''
+              height={202}
+              width={202}
+            />
+          </div>
+          <h2>Passionate</h2>
+          <p>
+            Each project starts with an in-depth brand research to ensure we
+            only create products that serve a purpose. We merge art, design, and
+            technology into exciting new solutions.
+          </p>
+        </div>
+        <div>
+          <div className='icon__container icon__container--large'>
+            <Image
+              src='/assets/home/desktop/illustration-resourceful.svg'
+              role='presentation'
+              alt=''
+              height={202}
+              width={202}
+            />
+          </div>
+          <h2>Resourceful</h2>
+          <p>
+            Everything that we do has a strategic purpose. We use an agile
+            approach in all of our projects and value customer collaboration. It
+            guarantees superior results that fulfill our clients&rsquo; needs.
+          </p>
+        </div>
 
-        <h2>Resourceful</h2>
-        <p>
-          Everything that we do has a strategic purpose. We use an agile
-          approach in all of our projects and value customer collaboration. It
-          guarantees superior results that fulfill our clients&rsquo; needs.
-        </p>
-
-        <h2>Friendly</h2>
-        <p>
-          We are a group of enthusiastic folks who know how to put people first.
-          Our success depends on our customers, and we strive to give them the
-          best experience a company can provide.
-        </p>
+        <div>
+          <div className='icon__container icon__container--large'>
+            <Image
+              src='/assets/home/desktop/illustration-friendly.svg'
+              role='presentation'
+              alt=''
+              height={202}
+              width={202}
+            />
+          </div>
+          <h2>Friendly</h2>
+          <p>
+            We are a group of enthusiastic folks who know how to put people
+            first. Our success depends on our customers, and we strive to give
+            them the best experience a company can provide.
+          </p>
+        </div>
       </section>
     </main>
   );
