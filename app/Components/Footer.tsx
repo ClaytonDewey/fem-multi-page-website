fe90b05da7d9325ee1e34bf31c7609932a4de2ec
ClaytonDewey <clay@dryadmedia.com>
2024-02-07 16:39:02 -0600

    refactor: Continue reworking footer

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -3,14 +3,14 @@ import Image from 'next/image';
 import Link from 'next/link';
 import logoImg from '../../public/assets/shared/desktop/logo-light.png';
 import ctaImg from '../../public/assets/shared/desktop/bg-pattern-call-to-action.svg';
-import { Navbar } from '.';
+import { Navbar, SocialLinks } from '.';
 
 type FooterProps = {};
 
 const Footer: React.FC<FooterProps> = () => {
   return (
     <footer className='footer'>
-      {/* <section className='cta__wrapper'>
+      {/* <div className='cta__wrapper'>
         <div className='cta__contact'>
           <Image src={ctaImg} alt='' role='' className='cta__bg' />
           <div className='cta__contact--body'>
@@ -28,23 +28,17 @@ const Footer: React.FC<FooterProps> = () => {
             </Link>
           </div>
         </div>
-      </section> */}
+      </div> */}
 
-      <div className='footer__body'>
-        <div className='footer__nav'>
-          <Link href='/' className='footer__nav--logo'>
-            <Image
-              src={logoImg}
-              alt='logo'
-              width='197'
-              height='27'
-              className='footer__nav--img'
-            />
-            <h1 className='visually-hidden'>DESIGNO</h1>
-          </Link>
-          <Navbar />
-        </div>
+      <div className='footer__nav'>
+        <Link href='/' className='footer__logo'>
+          <Image src={logoImg} alt='logo' width='197' height='27' />
+          <h2 className='visually-hidden'>DESIGNO</h2>
+        </Link>
+        <Navbar />
+      </div>
 
+      <div className='footer__content'>
         <address>
           <h2>Designo Central Office</h2>
           3886 Wellington Street
@@ -58,48 +52,7 @@ const Footer: React.FC<FooterProps> = () => {
           <p>M : contact@designo.co</p>
         </div>
 
-        <div className='footer__social'>
-          <Link href='/'>
-            <Image
-              src='/assets/shared/desktop/icon-facebook.svg'
-              alt='Facebook'
-              height={24}
-              width={24}
-            />
-          </Link>
-          <Link href='/'>
-            <Image
-              src='/assets/shared/desktop/icon-youtube.svg'
-              alt='YouTube'
-              height={24}
-              width={24}
-            />
-          </Link>
-          <Link href='/'>
-            <Image
-              src='/assets/shared/desktop/icon-twitter.svg'
-              alt='Twitter'
-              height={24}
-              width={24}
-            />
-          </Link>
-          <Link href='/'>
-            <Image
-              src='/assets/shared/desktop/icon-pinterest.svg'
-              alt='Pinterest'
-              height={24}
-              width={24}
-            />
-          </Link>
-          <Link href='/'>
-            <Image
-              src='/assets/shared/desktop/icon-instagram.svg'
-              alt='Instagram'
-              height={24}
-              width={24}
-            />
-          </Link>
-        </div>
+        <SocialLinks />
       </div>
     </footer>
   );
--- app/Components/index.ts
+++ app/Components/index.ts
@@ -2,3 +2,4 @@ export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as Locations } from './Locations';
 export { default as Navbar } from './Navbar';
+export { default as SocialLinks } from './SocialLinks';
