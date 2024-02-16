dc44162750865bb1cfe8e3088e8fe244a0e71118
ClaytonDewey <clay@dryadmedia.com>
2024-02-16 17:46:42 -0600

    Initial Commit

---
+++ 25-reactjs-interview-projects/src/App.js
@@ -0,0 +1,12 @@
+import Accordion from './components/accordion';
+
+function App() {
+  return (
+    <div className='App'>
+      {/* Accordion component */}
+      <Accordion />
+    </div>
+  );
+}
+
+export default App;
